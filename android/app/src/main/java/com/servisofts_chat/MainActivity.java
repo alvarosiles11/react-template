2e4d27021ebb77d0facf21c1ec1c45b67fb5763f
Alvaro Siles <61075383+alvarosiles11@users.noreply.github.com>
2022-03-04 21:44:22 -0400

    master

--- android/app/src/main/java/com/servisofts_chat/MainActivity.java
+++ android/app/src/main/java/com/servisofts_chat/MainActivity.java
@@ -1,4 +1,4 @@
-package com.servisofts_chat;
+package com.template;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "servisofts_chat";
+    return "template";
   }
 }
