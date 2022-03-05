7f6699549548d17aa992520f2f39cfe5cb13d4d6
Alvaro Siles <61075383+alvarosiles11@users.noreply.github.com>
2022-03-04 21:53:21 -0400

    ahi vamos

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -1,75 +1,18 @@
 import { SAssets } from 'servisofts-component'
 
-// import Logo, { ReactComponent as LogoW } from './svg/bateonIcon.svg';
-
-import LogoWithe, { ReactComponent as LogoWitheW } from './svg/logo_w.svg';
-import LogoBlack, { ReactComponent as LogoBlackW } from './svg/logo_b.svg';
-import LogoWithe1, { ReactComponent as LogoWithe1W } from './svg/logo_w_1.svg';
-import LogoBlack1, { ReactComponent as LogoBlack1W } from './svg/logo_b_1.svg';
-import LogoSSBlack, { ReactComponent as LogoSSBlackW } from './svg/logo_ss_b.svg';
-import LogoSSWithe, { ReactComponent as LogoSSWitheW } from './svg/logo_ss_w.svg';
-import Camara, { ReactComponent as CamaraW } from './svg/camara.svg';
-import Microfono, { ReactComponent as MicrofonoW } from './svg/microfono.svg';
-import Stick, { ReactComponent as StickW } from './svg/stick.svg';
-import AddChat, { ReactComponent as AddChatW } from './svg/addChat.svg';
-import CodeQR, { ReactComponent as CodeQRW } from './svg/codeQR.svg';
-
-
 import MenuEstado, { ReactComponent as MenuEstadoW } from './svg/menuEstado.svg';
 import MenuCall, { ReactComponent as MenuCallW } from './svg/menuCall.svg';
 import MenuCamara, { ReactComponent as MenuCamaraW } from './svg/menuCamara.svg';
 import MenuChats, { ReactComponent as MenuChatsW } from './svg/menuChats.svg';
 import MenuAjustes, { ReactComponent as MenuAjustesW } from './svg/menuAjustes.svg';
 
-
-
-import Menu000QR, { ReactComponent as Menu000QRW } from './svg/menu000QR.svg';
-import Menu000User, { ReactComponent as Menu000UserW } from './svg/menu000User.svg';
-
-
-import Menu001Star, { ReactComponent as Menu001StarW } from './svg/menu001Star.svg';
-import Menu002Lapto, { ReactComponent as Menu002LaptoW } from './svg/menu002Lapto.svg';
-import Menu003Key, { ReactComponent as Menu003KeyW } from './svg/menu003Key.svg';
-import Menu004Wasap, { ReactComponent as Menu004WasapW } from './svg/menu004Wasap.svg';
-import Menu005Notify, { ReactComponent as Menu005NotifyW } from './svg/menu005Notify.svg';
-import Menu006Datos, { ReactComponent as Menu006DatosW } from './svg/menu006Datos.svg';
-import Menu007Help, { ReactComponent as Menu007HelpW } from './svg/menu007Help.svg';
-
-
-
 const Assets: SAssets = {
     svg: {
-        "Logo": { Native: LogoWithe, Web: LogoWitheW },
-        "LogoWithe": { Native: LogoWithe, Web: LogoWitheW },
-        "LogoBlack": { Native: LogoBlack, Web: LogoBlackW },
-        "LogoSSBlack": { Native: LogoSSBlack, Web: LogoSSBlackW },
-        "LogoSSWithe": { Native: LogoSSWithe, Web: LogoSSWitheW },
-        "LogoWithe1": { Native: LogoWithe1, Web: LogoWithe1W },
-        "LogoBlack1": { Native: LogoBlack1, Web: LogoBlack1W },
-        "Camara": { Native: Camara, Web: CamaraW },
-        "Microfono": { Native: Microfono, Web: MicrofonoW },
-        "Stick": { Native: Stick, Web: StickW },
-        "AddChat": { Native: AddChat, Web: AddChatW },
-        "CodeQR": { Native: CodeQR, Web: CodeQRW },
-
-
         "MenuEstado": { Native: MenuEstado, Web: MenuEstadoW },
         "MenuCall": { Native: MenuCall, Web: MenuCallW },
         "MenuCamara": { Native: MenuCamara, Web: MenuCamaraW },
         "MenuChats": { Native: MenuChats, Web: MenuChatsW },
         "MenuAjustes": { Native: MenuAjustes, Web: MenuAjustesW },
-
-        "Menu000QR": { Native: Menu000QR, Web: Menu000QRW },
-        "Menu000User": { Native: Menu000User, Web: Menu000UserW },
-
-        "Menu001Star": { Native: Menu001Star, Web: Menu001StarW },
-        "Menu002Lapto": { Native: Menu002Lapto, Web: Menu002LaptoW },
-        "Menu003Key": { Native: Menu003Key, Web: Menu003KeyW },
-        "Menu004Wasap": { Native: Menu004Wasap, Web: Menu004WasapW },
-        "Menu005Notify": { Native: Menu005Notify, Web: Menu005NotifyW },
-        "Menu006Datos": { Native: Menu006Datos, Web: Menu006DatosW },
-        "Menu007Help": { Native: Menu007Help, Web: Menu007HelpW }
-
     }
 }
 
